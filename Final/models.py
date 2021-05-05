7484a862475f097b2a7d84d91204087eed88cf33
Conner <conmanmojo1@gmail.com>
2021-05-04 23:28:28 -0500

    added an HPC slot to db if we want to use for later

--- Final/models.py
+++ Final/models.py
@@ -19,7 +19,7 @@ class User(db.Model):
 	def serialize(self):
 		return {
 			'id': self.id,
-			'username': self.username	
+			'username': self.username,	
 		}
 
 	def __repr__(self):
@@ -34,7 +34,24 @@ class Profile(db.Model):
 	def serialize(self):
 		return {
 			'id': self.id,
-			'username': self.username
+			'username': self.username,
 		}
 	def __repr__(self):
 		return '<Profile id=%r>' % self.id
+
+
+class HPC(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'), nullable=False)
+	#status is based off of 1 or 0
+	# 1 meaning it is taken by another user
+	# 0 meaning the computer is free
+	# giving the option of storing the data here or we can update with Reactjs
+	status = db.Column(db.Integer, nullable=False)
+	profile = db.relationship('Profile', uselist=False, lazy=True)
+	
+	def serialize(self):
+		return {
+			'id' self.id,
+			'profile': self.profile.serialize(),
+		}
